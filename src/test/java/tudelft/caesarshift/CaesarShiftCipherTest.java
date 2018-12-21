503fa0f6b278968209b3789e3d0220950c6ca5f6
rwandastef <leffeblond2002@yahoo.com>
2018-12-21 14:50:54 +0100

    -- commit cesar test --

--- src/test/java/tudelft/caesarshift/CaesarShiftCipherTest.java
+++ src/test/java/tudelft/caesarshift/CaesarShiftCipherTest.java
@@ -1,5 +1,18 @@
 package tudelft.caesarshift;
 
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+
 public class CaesarShiftCipherTest {
+
+    @Test
+    public void testZeroShift(){
+        CaesarShiftCipher csc = new CaesarShiftCipher();
+        String result = csc.CaesarShiftCipher("hallo", 0);
+        Assertions.assertEquals(" blabla", result );
+
+        // result: hij append eerst de current karakter, dus krijg je een verdubbeling
+
+    }
 
 }
